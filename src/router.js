0dd6d7971bb93ad12a643d31f9e0bc847bbfbc9f
Eric Mok <emok032@gmail.com>
2017-01-11 20:41:39 -0500

    save

--- src/router.js
+++ src/router.js
@@ -3,9 +3,11 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+
+// Will not need anymore b/c are being dynamically loaded (see plain routes in - componentRoutes)
+// import ArtistDetail from './components/artists/ArtistDetail';
+// import ArtistCreate from './components/artists/ArtistCreate';
+// import ArtistEdit from './components/artists/ArtistEdit';
 
 const componentRoutes = {
   component: Home,
@@ -14,10 +16,30 @@ const componentRoutes = {
   childRoutes: [
     {
       path:'artists/new'
-      getComponent() { 
-      // by default, React-Router assumes that all desired components are already loaded up
+
+      // *by default, React-Router assumes that all desired components are already loaded up
       // but for Asynchronous loading, will need getComponent()s
-      
+      // React-Router expects to call cb with our module/component after loaded
+      getComponent(location, cb) { 
+        // React will fetch code for getComponent() and call the callback, cb
+        System.import('./components/artists/ArtistCreate')
+          .then(module => cb(null, module.default));
+        // remember System.import('') - webpack will automatically modify the bundle that is generated to split off the module that is called
+        // Gotcha's: cb(error argument,)
+      }
+    },
+    {
+      path:'artists/:id'
+      getComponent(location, cb) { 
+        System.import('./components/artists/Artist Detail')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path:'artists/:id/edit'
+      getComponent(location, cb) { 
+        System.import('./components/artists/ArtistEdit')
+          .then(module => cb(null, module.default));
       }
     }
   ]
@@ -25,14 +47,7 @@ const componentRoutes = {
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
